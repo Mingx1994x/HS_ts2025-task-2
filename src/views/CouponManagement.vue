35d6592f8108735f9899e379b3e4054b7a5d49a6
Mingx1994x <mggxmingx@gmail.com>
2025-12-12 16:39:24 +0800

    test:新增後台優惠券管理頁面與 取得優惠券 api 測試

---
+++ src/views/CouponManagement.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { apiGetCoupons } from '@/api/coupon';
+import { onMounted, ref } from 'vue';
+
+  const currentPage = ref<string>('1')
+  const getCoupons=async()=>{
+    try {
+      const res=await apiGetCoupons({
+        page:currentPage.value
+      })
+      console.log(res);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  onMounted(()=>{
+    getCoupons()
+  })
+</script>
+<template>
+  <div class="d-flex justify-content-end align-items-center mb-4">
+    <button type="button" class="btn btn-dark rounded-lg px-4 py-2">
+      <i class="fas fa-plus me-2"></i>新增優惠券
+    </button>
+  </div>
+</template>
