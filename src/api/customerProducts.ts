5d62c9bb3270762d138baaee4c87a221d83fdae4
Mingx1994x <mggxmingx@gmail.com>
2025-12-21 00:13:51 +0800

    chore: 使用 prettier 格式化 customerProduct.ts

--- src/api/customerProducts.ts
+++ src/api/customerProducts.ts
@@ -1,21 +1,31 @@
-import axios from "axios"
+import axios from 'axios'
 
-import type { AxiosResponse } from "axios"
-import type { TGetProductsResponse,TGetProductByIdResponse, TGetProductsAllResponse, TProduct } from "@/types/customer/product"
+import type { AxiosResponse } from 'axios'
+import type {
+  TGetProductsResponse,
+  TGetProductByIdResponse,
+  TGetProductsAllResponse,
+  TProduct,
+} from '@/types/customer/product'
 
-const {VITE_BASE_URL,VITE_API_PATH} =import.meta.env
+const { VITE_BASE_URL, VITE_API_PATH } = import.meta.env
 
 export const hexStoreApi = axios.create({
   baseURL: VITE_BASE_URL,
 })
 
+export const getProductsAll = (): Promise<AxiosResponse<TGetProductsAllResponse>> =>
+  hexStoreApi.get(`/v2/api/${VITE_API_PATH}/products/all`)
 
-export const getProductsAll=():Promise<AxiosResponse<TGetProductsAllResponse>>=>(hexStoreApi.get(`/v2/api/${VITE_API_PATH}/products/all`))
+export const getProducts = (params: {
+  page?: string
+  category?: string
+}): Promise<AxiosResponse<TGetProductsResponse>> =>
+  hexStoreApi.get(`/v2/api/${VITE_API_PATH}/products`, { params })
 
-export const getProducts=(params:{page?:string,category?:string}):Promise<AxiosResponse<TGetProductsResponse>>=>(hexStoreApi.get(`/v2/api/${VITE_API_PATH}/products`,{params}))
+export const getProductById = (id: string): Promise<AxiosResponse<TGetProductByIdResponse>> =>
+  hexStoreApi.get(`/v2/api/${VITE_API_PATH}/product/${id}`)
 
-export const getProductById=(id:string):Promise<AxiosResponse<TGetProductByIdResponse>>=>(hexStoreApi.get(`/v2/api/${VITE_API_PATH}/product/${id}`))
-
-export function getProductsCategories(data:TProduct[]):string[]{
-  return [...new Set(data.map(item => item.category))]
+export function getProductsCategories(data: TProduct[]): string[] {
+  return [...new Set(data.map((item) => item.category))]
 }
