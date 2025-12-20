a796cb98320dc1bf07c20839e34f3169ac950306
Mingx1994x <mggxmingx@gmail.com>
2025-12-20 11:00:32 +0800

    fix: 修正取得商品 API 錯誤 不～多打了一個 s，害我抓蟲抓了半小時

--- src/api/customerProducts.ts
+++ src/api/customerProducts.ts
@@ -14,7 +14,7 @@ export const getProductsAll=():Promise<AxiosResponse<TGetProductsAllResponse>>=>
 
 export const getProducts=(params:{page?:string,category?:string}):Promise<AxiosResponse<TGetProductsResponse>>=>(hexStoreApi.get(`/v2/api/${VITE_API_PATH}/products`,{params}))
 
-export const getProductById=(id:string):Promise<AxiosResponse<TGetProductByIdResponse>>=>(hexStoreApi.get(`/v2/api/${VITE_API_PATH}/products/${id}`))
+export const getProductById=(id:string):Promise<AxiosResponse<TGetProductByIdResponse>>=>(hexStoreApi.get(`/v2/api/${VITE_API_PATH}/product/${id}`))
 
 export function getProductsCategories(data:TProduct[]):string[]{
   return [...new Set(data.map(item => item.category))]
